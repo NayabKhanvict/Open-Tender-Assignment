84abb9c89f35cb073db91ca2be703560713a8439
NayabKhanvict <nayabkhanvict@gmail.com>
2023-03-30 02:01:07 +0500

    created route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppRoutes from './routes/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-         Sufyan let's start from here
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppRoutes />
+    </>
   );
 }
 
